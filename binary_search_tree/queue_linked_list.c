71d2090b3a063bed664b61a6e6b3c30f3be46868
aloe <120464393+aloe-lue@users.noreply.github.com>
2025-11-04 16:04:08 +0800

    untested queue linked list for bst
    use this for level order
    this will help with return an array from level order function

--- binary_search_tree/queue_linked_list.c
+++ binary_search_tree/queue_linked_list.c
@@ -5,141 +5,62 @@
 #include "queue_linked_list.h"
 #include "binary_search_tree.h"
 
-q_list_node *create_q_list_node(int *data)
+ql_node *create_ql_node(bst_node *node)
 {
-        q_list_node *qln = (q_list_node *)malloc(sizeof(q_list_node));
-        if (qln == NULL) {
-                puts("malloc error: create_q_list_node func. \n");
-                return qln;
-        }
-        for (int i = 0; i < 2; i++)
-                qln->data[i] = data[i];
-        qln->next = NULL;
-        return qln;
-}
-
-q_linked_list *create_q_linked_list()
-{
-        q_linked_list *QLL = (q_linked_list *)malloc(sizeof(q_linked_list));
-        QLL->head = NULL;
-        QLL->tail = NULL;
-        QLL->q_ll_len = 0;
-        return QLL;
-}
-
-void enqueue_q_list_node(q_linked_list *QLL, int *data)
-{
-        q_list_node *new_q_list_node = create_q_list_node(data);
-        if (new_q_list_node == NULL) {
-                puts("malloc failed on enq func. \n");
-                return;
-        }
-        if (QLL->tail == NULL) {
-                QLL->head = QLL->tail = new_q_list_node;
-                QLL->q_ll_len++;
-                return;
-        }
-        QLL->tail->next = new_q_list_node;
-        QLL->tail = new_q_list_node;
-        QLL->q_ll_len++;
-        return;
-}
-
-int *dequeue_q_list_node(q_linked_list *QLL)
-{       
-        int *value;
-        if (QLL->q_ll_len == 0) {
-                value = (int *)malloc(2 * sizeof(int));
-                if (value == NULL) {
-                        fprintf(stderr, "malloc failed: dequeue func");
-                        exit(EXIT_FAILURE);
-                }
-                for (int i = 0; i < 2; i++)
-                        value[i] = -1;
-                return value;
-        }
-        q_list_node *tmp = QLL->head;
-        QLL->head = tmp->next;
-        if (QLL->head == NULL) {
-                QLL->tail = NULL;
-        }
-        value = (int *)malloc(2 * sizeof(int));
-        if (value == NULL) {
-                fprintf(stderr, "malloc failed: dequeue func");
+        ql_node *q_node = (ql_node *)malloc(sizeof(ql_node));
+        if (q_node == NULL) {
+                fprintf(stderr, "create_bst_qll func: malloc failed.\n");
                 exit(EXIT_FAILURE);
         }
-        for (int i = 0; i < 2; i++)
-                value[i] = tmp->data[i];
-        QLL->q_ll_len--;
+        q_node->node = node;
+        q_node->next = NULL;
+        return q_node;
+}
+
+bst_qll *create_bst_qll()
+{
+        bst_qll *Q = (bst_qll *)malloc(sizeof(bst_qll));
+        if (Q == NULL) {
+                fprintf(stderr, "create_bst_qll func: malloc failed.\n");
+                exit(EXIT_FAILURE);
+        }
+        Q->tail = NULL;
+        Q->head = NULL;
+        Q->len = 0;
+        return Q;
+}
+
+void bst_qll_enqueue(bst_qll *Q, bst_node *node) 
+{
+        ql_node *new_ql_node = create_ql_node(node);
+        if (Q->len == 0) {
+                Q->head = Q->tail = new_ql_node;
+                Q->len++;
+                return;
+        }
+        Q->tail->next = new_ql_node;
+        Q->tail = new_ql_node;
+        Q->len++;
+}
+
+bst_node *bst_qll_dequeue(bst_qll *Q)
+{
+        if (Q->len == 0) {
+                fprintf(stderr, "bst_qll_dequeue: Q empty. \n");
+                exit(EXIT_FAILURE);
+        }
+        ql_node *tmp = Q->head;
+        Q->head->next = tmp->next;
+        if (Q->head == NULL) {
+                Q->tail = NULL;
+        }
+        bst_node *dqd_node = (bst_node *)malloc(sizeof(bst_node));
+        if (dqd_node == NULL) {
+                fprintf(stderr, "bst_qll_dequeue: malloc failed. \n");
+                exit(EXIT_FAILURE);
+        }
+        dqd_node = tmp->node;
+        Q->len--;
         free(tmp);
-        return value;
+        return dqd_node;
 }
-
-int size_q_list_node(q_linked_list *QLL)
-{
-        return QLL->q_ll_len;
-}
-
-int *peek_q_list_node_s(q_linked_list *QLL)
-{
-        return QLL->head->data;
-}
-
-void destroy_q_list_node(q_list_node *q)
-{
-        if (q->next == NULL) {
-                return;
-        }
-        destroy_q_list_node(q->next);
-        free(q);
-}
-
-/**
- * down here is for queue for bst
- */
-bst_q_tree *bst_q_create()
-{
-        bst_q_tree *q_tree = (bst_q_tree *)malloc(sizeof(bst_q_tree));
-        if (q_tree == NULL) {
-                fprintf(stderr, "dequeue_bst_q_node: q is empty.");
-                exit(EXIT_FAILURE);
-        }
-        q_tree->head = NULL;
-        q_tree->tail = NULL;
-        q_tree->size = 0;
-        return q_tree;
-}
-
-void enqueue_bst_q_node(bst_q_tree *tree, bst_node *node) 
-{
-        if (tree->tail == NULL) {
-                tree->tail = tree->head = node;
-                tree->size++;
-                return;
-        }
-        tree->tail->next = node;
-        tree->tail = node;
-        tree->size++;
-}
-
-bst_node *dequeue_bst_q_node(bst_q_tree *tree)
-{
-        if (tree->size == 0) {
-                fprintf(stderr, "dequeue_bst_q_node: q is empty.");
-                exit(EXIT_FAILURE);
-        }
-        bst_node *tmp = tree->head;
-        tree->head = tmp->next;
-        if (tree->head == NULL) {
-                tree->tail = NULL;
-        }
-        bst_node *data = (bst_node *)malloc(sizeof(bst_node));
-        data->key = tmp->key;
-        data->right = tmp->right;
-        data->left = tmp->left;;
-        tree->size--;
-        free(tmp);
-        return data;
-}
-
-
--- binary_search_tree/queue_linked_list.h
+++ binary_search_tree/queue_linked_list.h
@@ -4,104 +4,49 @@
 #include "binary_search_tree.h"
 #include "linked_list.h"
 
-typedef struct q_list_node {
-        int data[2];
-        struct q_list_node *next;
-} q_list_node;
-
-typedef struct q_linked_list {
-        q_list_node *head;
-        q_list_node *tail;
-        int q_ll_len;
-} q_linked_list;
-
 /**
- * init a q linked list
- * @params nothing
- * returns q_linked_list accessible
- */
-q_linked_list *create_q_linked_list();
-/**
- * create q list node
+ * follows first in first out
  *
- * @params int *data
- * returns q_list_node
+ * tail pointer
  */
-q_list_node *create_q_list_node(int *data);
-/**
- * add to the front of the q_list_node head
- * update the tail of the q_list_node tail
- * if it's not NULL
- *
- * @params q_linked_list
- * returns void
- */
-void enqueue_q_list_node(q_linked_list *QLL, int *data);
+typedef struct ql_node {
+        bst_node *node;
+        struct ql_node *next;
+} ql_node;
+
+typedef struct bst_qll {
+        ql_node *tail;
+        ql_node *head;
+        int len;
+} bst_qll;
 
 /**
- * get the tail of the queue
- * remove the tail of the queue
- * update the tail of the queue
- *
- * @params q_linked_list
- * returns a q_list_node of the removed q
- */
-int *dequeue_q_list_node(q_linked_list *QLL);
-
-/**
- * get the q_ll_len
+ * create queue list node
  * 
- * @params q_linked_list
+ * returns ql_node
  */
-int size_q_list_node(q_linked_list *q);
-
-/*
- * print the front q_list_node
- *
- * @params q_linked_list q
- * returns nothing
- */
-int *peek_q_list_node_s(q_linked_list *q);
+ql_node *create_ql_node(bst_node *node);
 
 /**
- * you want to destroy all of it after use weird phrasing
+ * yeah so create bst queue linked list
  *
- * @params q linked list Qll
+ * returns bst_qll
  */
-
-void destroy_q_list_node(q_list_node *q);
-
-/**
- * def struct for tree
- */
-typedef struct bst_q_tree {
-        bst_node *head;
-        bst_node *tail;
-        int size;
-} bst_q_tree;
-/**
- * this create the queue struct
- *
- * @params
- */
-bst_q_tree *bst_q_create();
-/**
- * bst node have next left right and parent pointer
- * and so in this case I will use enqueue to display
- * or destroy the tree with this queue weird
- *
- * @params bst_tree
- * returns nothing 
- */
-void enqueue_bst_q_node(bst_q_tree *tree, bst_node *node);
+bst_qll *create_bst_qll();
 
 /**
- * destroy the bst node of the head
+ * enqueue bst node in the bst_qll
  *
- * @params bst_tree
- * returns the bst node of the head so free it after use
- *
+ * @params bst_qll and bst_node
  */
-bst_node *dequeue_bst_q_node(bst_q_tree *tree);
+void bst_qll_enqueue(bst_qll *Q, bst_node *node);
+
+/**
+ * dequeue ql node from bst_qll
+ *
+ * @params bst_qll
+ */
+bst_node *bst_qll_dequeue(bst_qll *Q);
+
 
 #endif
